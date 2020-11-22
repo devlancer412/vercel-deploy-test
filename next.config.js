40fa9056260ffc279d7a775fc8410ad217fd2c17
thomasin <thomasinabraham@me.com>
2020-11-22 16:06:52 +0100

    Upgrade Next and React

--- next.config.js
+++ next.config.js
@@ -1,17 +1,15 @@
 const withFonts = require('next-fonts')
 
 module.exports = withFonts({
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
   webpack: config => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
       fs: 'empty',
     }
-
-    // // Add support for requiring css files
-    // config.module.rules.push({
-    //   test: /\.s[ac]ss$/i,
-    //   use: ['css-loader', 'sass-loader'],
-    // })
 
     config.module.rules.push({
       test: /\.svg$/,
